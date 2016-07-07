330585a8bbeeb7b13fb627e21b0ab54a2a8aa287
Juan Jose Herrera Espinoza <juan.herrerae@gmail.com>
2016-07-07 14:30:37 -0500

    Missing commit

--- Acme.Biz/Product.cs
+++ Acme.Biz/Product.cs
@@ -108,8 +108,23 @@ namespace Acme.Biz
         /// </summary>
         /// <param name="markupPercent">Percent used to mark up the cost.</param>
         /// <returns></returns>
-        public decimal CalculateSuggestedPrice(decimal markupPercent) =>
-             this.Cost + (this.Cost * markupPercent / 100);
+        public OperationResultDecimal CalculateSuggestedPrice(decimal markupPercent)
+        {
+            var message = "";
+            if(markupPercent <= 0)
+            {
+                message = "Invalid markup percentage";
+            }
+            else if(markupPercent < 10)
+            {
+                message = "Below recommended markup percentage";
+            }
+
+            var value = this.Cost + (this.Cost * markupPercent / 100);
+
+            var operationalResult = new OperationResultDecimal(value, message);
+            return operationalResult;
+        }
 
         public override string ToString()
         {
